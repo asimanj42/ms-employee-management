75b9c689415986d0945f79397c31bf558047c869
Asiman42 <asimanmemmedli123321@gmail.com>
2023-07-31 23:41:33 +0400

    Rest Template,Web Client,Feign Client method added

--- employee-service/src/main/java/az/company/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/az/company/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,12 +8,12 @@ import az.company.employeeservice.model.ApiResponseDto;
 import az.company.employeeservice.model.DepartmentDto;
 import az.company.employeeservice.model.EmployeeDto;
 import az.company.employeeservice.repository.EmployeeRepository;
+import az.company.employeeservice.service.ApiClient;
 import az.company.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import static az.company.employeeservice.utility.ConstantErrorMessage.EMPLOYEE_ALREADY_EXISTS;
 
@@ -26,7 +26,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper employeeMapper;
-    private final RestTemplate restTemplate;
+    //    private final RestTemplate restTemplate;
+    //    private final WebClient webClient;
+    private final ApiClient apiClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -42,16 +44,42 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
 
+//    @Override
+//    public ApiResponseDto getEmployeeById(Long employeeId) {
+//        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeeId));
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
+//        DepartmentDto departmentDto = responseEntity.getBody();
+//        EmployeeDto employeeDto = employeeMapper.entityToEmployeeDto(employee);
+//        ApiResponseDto apiResponseDto = new ApiResponseDto();
+//        apiResponseDto.setDepartment(departmentDto);
+//        apiResponseDto.setEmployee(employeeDto);
+//        return apiResponseDto;
+//    }
+
+//    @Override
+//    public ApiResponseDto getEmployeeById(Long employeeId) {
+//        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeeId));
+//       DepartmentDto departmentDto = webClient.get()
+//                .uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
+//                .retrieve()
+//                .bodyToMono(DepartmentDto.class)
+//                .block();
+//        EmployeeDto employeeDto = employeeMapper.entityToEmployeeDto(employee);
+//        ApiResponseDto apiResponseDto = new ApiResponseDto();
+//        apiResponseDto.setDepartment(departmentDto);
+//        apiResponseDto.setEmployee(employeeDto);
+//        return apiResponseDto;
+//    }
+
+
     @Override
     public ApiResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeeId));
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
-        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
         EmployeeDto employeeDto = employeeMapper.entityToEmployeeDto(employee);
         ApiResponseDto apiResponseDto = new ApiResponseDto();
-        apiResponseDto.setDepartmentDto(departmentDto);
-        apiResponseDto.setEmployeeDto(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+        apiResponseDto.setEmployee(employeeDto);
         return apiResponseDto;
     }
-
 }

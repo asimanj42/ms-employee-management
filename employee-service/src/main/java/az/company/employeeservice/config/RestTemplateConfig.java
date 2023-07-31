2309a12d6b421f48b26b0555de5708816d3fa8dc
Asiman42 <asimanmemmedli123321@gmail.com>
2023-07-31 23:38:09 +0400

    Rest Template added

--- employee-service/src/main/java/az/company/employeeservice/config/RestTemplateConfig.java
+++ employee-service/src/main/java/az/company/employeeservice/config/RestTemplateConfig.java
@@ -1,15 +1,17 @@
-package az.company.employeeservice.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-@Configuration
-public class RestTemplateConfig {
-
-    @Bean
-    public RestTemplate restTemplate() {
-
-        return new RestTemplate();
-    }
-}
+//package az.company.employeeservice.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Configuration
+//public class RestTemplateConfig {
+//
+//    @Bean
+//    public RestTemplate restTemplate() {
+//
+//        return new RestTemplate();
+//    }
+//
+//
+//}

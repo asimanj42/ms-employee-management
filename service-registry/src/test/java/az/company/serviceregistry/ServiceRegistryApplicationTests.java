3bf6dc46fcbb14fd8cff7cdee25c2899bf13715e
Asiman42 <asimanmemmedli123321@gmail.com>
2023-08-01 14:19:02 +0400

    Service Registry added

---
+++ service-registry/src/test/java/az/company/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package az.company.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
